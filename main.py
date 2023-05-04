ebeb6ece24de4132c1d3161f7c287921513ccd47
DACHXY <danny10132024@gmail.com>
2023-05-04 09:20:12 +0800

    增加 home.html 的 accordion

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from typing import Optional, Union
 from flask import Flask, render_template, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 from server.backend import Backend
 import subprocess
 
 app : Flask = Flask(__name__, template_folder="templates")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db : SQLAlchemy = SQLAlchemy(app)
 
 ## Config
 BACKEND_PATH = r"C:\\Users\danny\Documents\DN\Projects\Activer\backend"
@@ -22,24 +25,28 @@ backend_server : Backend = Backend(
 def index():
     return render_template(HOME_PATH), 200
 
-@app.route("/backend/start", methods=["GET"])
+@app.route("/start", methods=["GET"])
 def start_backend():
     status : Optional[subprocess.Popen] = backend_server.run()
     if status is None:
-        return jsonify(status_code=200, msg="後端已經在運行"), 299
+        return jsonify(status_code=200, msg="後端已經在運行"), 200
     return jsonify(status_code=200, msg="後端開始運行"), 200
 
-@app.route("/backend/status", methods=["GET"])
+@app.route("/status", methods=["GET"])
 def get_backend_status():
     status : str = "running" if backend_server.is_server_alive() else "stopped"
     return jsonify(status_code=200, status=status), 200
 
-@app.route("/backend/stop", methods=["GET"])
+@app.route("/stop", methods=["GET"])
 def stop_backend():
     status : bool = backend_server.stop()
     if status:
         return jsonify(status_code=200, msg="後端伺服器已停止運作"), 200
     return jsonify(status_code=200, msg="後端伺服器沒有運作或發生錯誤"), 200
+
+@app.route("/all")
+def get_all_server():
+    return None
 
 if __name__ == "__main__":
     app.run()
